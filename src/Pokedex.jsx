a31bc1f4ee09383247c6638c2ea8bd13436e8d2e
Adiyat <metaladiyat@gmail.com>
2021-05-09 20:45:42 +0800

    Adding data to layout and style them

--- src/Pokedex.jsx
+++ src/Pokedex.jsx
@@ -1,48 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Grid,
   AppBar,
   Toolbar,
   Card,
   CardContent,
+  CardMedia,
   Container,
+  CircularProgress,
+  Typography,
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+
+// API
+import mockData from "./mockData";
 
 const useStyles = makeStyles({
   container: {
     marginTop: 20,
   },
+  pokeSprite: {
+    margin: "auto",
+  },
+  pokeCard: {
+    "&:hover": {
+      background: "#ddd",
+      cursor: "pointer",
+    },
+  },
 });
 
-const getPokemonCard = () => {
-  return (
-    <Grid item xs={12} sm={4}>
-      <Card>
-        <CardContent>Ini Item</CardContent>
-      </Card>
-    </Grid>
-  );
-};
-
-const Pokedex = () => {
+const Pokedex = (props) => {
   const classes = useStyles();
+  const { history } = props;
+  const [pokemonData, setPokemonData] = useState(mockData);
+  const firstCharUppercase = (string) =>
+    string.charAt(0).toUpperCase() + string.slice(1);
+
+  // return pokemon card
+  const getPokemonCard = (pokemonID) => {
+    const { id, name } = pokemonData[pokemonID];
+    const sprite = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+    return (
+      <Grid item xs={12} sm={4} key={pokemonID}>
+        <Card
+          onClick={() => history.push(`/${pokemonID}`)}
+          className={classes.pokeCard}
+        >
+          <CardMedia
+            image={sprite}
+            style={{ width: "130px", height: "130px" }}
+            className={classes.pokeSprite}
+          />
+          <CardContent>
+            <Typography align="center">{firstCharUppercase(name)}</Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    );
+  };
   return (
     <React.Fragment>
       <AppBar position="static">
         <Toolbar />
       </AppBar>
-      <Container className={classes.container}>
-        <Grid container spacing={2}>
-          {getPokemonCard()}
-          {getPokemonCard()}
-          {getPokemonCard()}
-          {getPokemonCard()}
-          {getPokemonCard()}
-          {getPokemonCard()}
-          {getPokemonCard()}
-        </Grid>
-      </Container>
+      {pokemonData ? (
+        <Container className={classes.container}>
+          <Grid container spacing={2}>
+            {Object.keys(pokemonData).map((pokemonID) =>
+              getPokemonCard(pokemonID)
+            )}
+          </Grid>
+        </Container>
+      ) : (
+        <CircularProgress />
+      )}
     </React.Fragment>
   );
 };
--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -1,8 +1,93 @@
-import React from "react";
+import {
+  Card,
+  CardContent,
+  Typography,
+  Container,
+  Grid,
+  makeStyles,
+} from "@material-ui/core";
+import { RestaurantMenu } from "@material-ui/icons";
+import React, { useState } from "react";
+import mockData from "./mockData";
+
+const useStyles = makeStyles((theme) => ({
+  pokeContainer: {
+    paddingTop: "16px",
+    [theme.breakpoints.up("md")]: {
+      height: "100vh",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      paddingTop: "0",
+    },
+  },
+  imgCenter: {
+    maxWidth: "300px",
+    maxHeight: "300px",
+    margin: "10px auto",
+    display: "block",
+  },
+  pokeTitle: {
+    position: "relative",
+    display: "inline-block",
+    margin: "0 auto",
+    [theme.breakpoints.down("md")]: {
+      fontSize: "3rem",
+    },
+  },
+}));
 
 const Pokemon = (props) => {
+  const classes = useStyles();
   const pokemonID = props.match.params.pokemonID;
-  return <div>{`Ini Pokemon nomor #${pokemonID}`}</div>;
+  const [pokemon, setPokemon] = useState(mockData[pokemonID]);
+
+  /**
+   * return card that contains pokemon information
+   */
+  const generatePokemonJSX = () => {
+    const { name, id, species, height, weight, types, sprites } = pokemon;
+    const fullImgUrl = `https://pokeres.bastionbot.org/images/pokemon/${pokemonID}.png`;
+    const { front_default } = sprites;
+    console.log(pokemon);
+    return (
+      <Card>
+        <CardContent>
+          <Grid container spacing={2}>
+            <Grid item md={6} xs={12} style={{ textAlign: "center" }}>
+              <Typography variant="h2" className={classes.pokeTitle}>
+                {name}
+                <img
+                  src={front_default}
+                  alt={name}
+                  style={{ position: "absolute", top: "-35px", right: "-80px" }}
+                />
+              </Typography>
+              <img src={fullImgUrl} alt={name} className={classes.imgCenter} />
+            </Grid>
+            <Grid item md={6} xs={12}>
+              <Typography variant="h6" style={{ fontWeight: "bold" }}>
+                Pokemon Info
+              </Typography>
+              <Typography variant="h6">ID : {id}</Typography>
+              <Typography variant="h6">Species : {species.name}</Typography>
+              <Typography variant="h6">Height : {height}</Typography>
+              <Typography variant="h6">Weight : {weight}</Typography>
+              <Typography variant="h6">Types : {types[0].type.name}</Typography>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+    );
+  };
+
+  return (
+    <React.Fragment>
+      <Container maxWidth="md" classes={{ root: classes.pokeContainer }}>
+        {generatePokemonJSX()}
+      </Container>
+    </React.Fragment>
+  );
 };
 
 export default Pokemon;
